e02546815875c2c5372807d1eac62e272ef2c43f
jschumacher <jschumacher@thorntontomasetti.com>
2013-01-30 20:54:23 -0500

    Hello PAE810. This is the beginning
    The very first version of SharePointConnect

---
+++ SharePointConnect/SharePointConnect/SharePointConnect.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Xml;
+
+
+
+namespace SharePointConnect
+{
+    public class SharePointConnector
+    {
+
+        ///////FIELDS
+        private string myPassWord, myUserName;
+        private bool myInitialized;
+
+        ///////PROPERTIES
+        public string url { get; set; }
+        public PAE810SharePoint.Lists SPConnection { get; set; }
+
+
+        ////////CONSTRUCTOR
+     //   public SharePointConnector();
+        public SharePointConnector(string URL, string UserName, string PassWord)
+        {
+            url = URL;
+            myUserName = UserName;
+            myPassWord = PassWord;
+            myInitialized = true;
+        }
+        
+        ////////METHODS
+        public bool connectToSP()
+        {
+            bool SPconnect = false;
+            
+            SPConnection = new PAE810SharePoint.Lists();
+            SPConnection.Url = url;
+
+            // if you're an outside the network user
+            SPConnection.Credentials = new NetworkCredential(myUserName, myPassWord);
+
+            // if you're an inside the network user with integrated security
+            // myConnection.Credentials = CredentialCache.DefaultNetworkCredentials;
+
+            //see if we can successfully make web service calls
+            try
+            {
+                XmlNode listCollection = SPConnection.GetListCollection();
+                SPconnect = true;
+            }
+            catch (Exception)
+            {
+            }
+            return SPconnect;
+        }
+
+
+        //create new list in SP(ListName, [List Attributes());
+        //ELCIN
+
+        //get List attributes(ListName, Array of AttributeNames);
+        //NACIEM
+
+        //get ListNames () 
+        //---returns a list of all names
+        //MICHAEL
+
+        //get ListItems(ListName)
+        //--return List of XElements 
+        //SAM
+
+        //get ListTitle(ListName)
+        //--return returns list with strings of item titles
+        //OLEG
+
+        //addItem(string ListName, XElement item)
+        //--adds item to spefici list
+        //GIORGOS
+
+        // get list length(ListName)
+        //returns integer
+        //MICHAEL
+
+        //public List<string> getItemAttributeValues(XElement item, params string[] attributeNames)
+        //{
+        //ALEX
+
+        //}
+    }
+}
---
+++ SharePointConnect/UnitTest/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SharePointConnect;
+
+
+namespace UnitTest
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine(UnitTest1());
+            Console.ReadLine();
+        }
+
+        private static string UnitTest1()
+        {
+            string myURL, myUser, myPassword;
+            myURL = "https://partners.myskanska.com/usa/teams/sit/hit/_vti_bin/lists.asmx";
+            myUser = "j.schumacher";
+            //myPassword = "Password1234";
+
+            Console.Write("Enter Password: ");
+            myPassword = "";
+            ConsoleKeyInfo keystroke=new ConsoleKeyInfo();
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                keystroke = Console.ReadKey(true);
+                switch (keystroke.Key)
+                {
+                    case ConsoleKey.Backspace:
+                        {
+                            //in leu of taking back from the paswordstring
+                            Console.Write("BACKSPACE");
+                            break;
+                        }
+                    case ConsoleKey.Enter:
+                        {
+                            keepGoing = false;
+                            Console.WriteLine();
+                            break;
+                        }
+                    default:
+                        {
+                            myPassword += keystroke.KeyChar;
+                            Console.Write("*");
+                            break;
+                        }
+                }
+            }
+
+            SharePointConnector mytestConnection = new SharePointConnector(myURL,myUser,myPassword);
+            if (mytestConnection.connectToSP() == true)
+            {
+                //Console.WriteLine("Hurray");
+                return "UnitTest1 = SUCCESS";
+            }
+            else
+            {
+                //Console.WriteLine("Nooooooo");
+                return "UnitTest1 = FAILURE";
+            }
+
+           
+        }
+    }
+}

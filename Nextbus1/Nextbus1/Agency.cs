750ccd94dccc2f4d3ae4a1e993c01eae880e85d3
jschumacher <jschumacher@thorntontomasetti.com>
2013-02-08 21:13:46 -0500

    added Nextbus example

---
+++ Nextbus1/Nextbus1/Agency.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+
+namespace Nextbus1
+{
+    public class Agency
+    {
+        //<agency tag="charles-river" title="Charles River TMA - EZRide" shortTitle="Charles River EZRide" regionTitle="Massachusetts"/>
+
+        public string tag, title, shortTitle, regionTitle;
+        public List<Route> busRoutes;
+        
+        //constructor
+        public Agency(XmlElement XMLNode)
+        {
+            if (XMLNode.Attributes["tag"] != null) tag = XMLNode.Attributes["tag"].Value;
+            if (XMLNode.Attributes["title"] != null) title = XMLNode.Attributes["title"].Value;
+            if (XMLNode.Attributes["shortTitle"] != null) shortTitle = XMLNode.Attributes["shortTitle"].Value;
+            if (XMLNode.Attributes["regionTitle"] != null) regionTitle = XMLNode.Attributes["regionTitle"].Value;
+        }
+    }
+
+
+}
---
+++ Nextbus1/Nextbus1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Xml;
+
+namespace Nextbus1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create the web request  
+            HttpWebRequest myRequest = WebRequest.Create("http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList") as HttpWebRequest;
+            WebResponse myResponse = myRequest.GetResponse();
+            Stream myStream = myResponse.GetResponseStream();
+            StreamReader myStreamreader = new StreamReader(myStream);
+            string myStreamResponse = myStreamreader.ReadToEnd();
+            XmlDocument myXMLDoc = new XmlDocument();
+            myXMLDoc.LoadXml(myStreamResponse);
+            XmlElement myDocElement = myXMLDoc.DocumentElement;
+            List<Agency> myAgencies = new List<Agency>();
+
+            foreach (XmlElement childNode in myDocElement.ChildNodes)
+            {
+                foreach (XmlAttribute myAtt in childNode.Attributes)
+                {
+                    Console.WriteLine(myAtt.Name + " my AttName | " + myAtt.Value + " my AttValue");
+                   
+                }
+                Console.WriteLine();
+                //Console.WriteLine(childNode.Attributes["title"].Value);
+                Agency myAgency = new Agency(childNode);
+                myAgencies.Add(myAgency);
+            }
+
+            Console.WriteLine(myAgencies.Count + " agencies loaded");
+            Console.ReadLine();
+        }
+    }
+}
